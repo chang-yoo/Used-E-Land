0b9add75779cbc2caa3de9b1fb9efd3032c324cf
Changhyun Yoo <chyoo0428@gmail.com>
2022-06-06 19:27:47 +0000

    upload page completed styling

--- client/pages/upload.jsx
+++ client/pages/upload.jsx
@@ -26,48 +26,74 @@ export default class Upload extends React.Component {
         <div className="row">
           <div className="column-half">
             <div className="image-container">
-              <image></image>
+              <img src="images/image-placeholder.png"></img>
+              <button className="search-button margin-top-1rem">Search</button>
             </div>
-            <div>
-              <label htmlFor="condition">Condition</label>
-              <select name="condition" required>
-                <option value="very used">very used</option>
-                <option value="used">used</option>
-                <option value="like new">like new</option>
-                <option value="brand new">brand new</option>
-              </select>
-              <input
-              required
-              id="location"
-              type="text"
-              name="location"
-              onChange={this.handleChange}
-              />
-              <input
-              required
-              id="price"
-              type="text"
-              name="price"
-              onChagne={this.handleChange}
-              />
+            <div className="test">
+              <div className="condition-container">
+                <label id="font-color" htmlFor="condition">Condition: </label>
+                <select className="condition" name="condition" required>
+                  <option className="select" value="very used">very used</option>
+                  <option className="select" value="used">used</option>
+                  <option className="select" value="like new">like new</option>
+                  <option className="select" value="brand new">brand new</option>
+                </select>
+              </div>
+              <div className="location-container">
+                <input
+                required
+                id="location"
+                type="text"
+                name="location"
+                onChange={this.handleChange}
+                placeholder='location'
+                />
+              </div>
+              <div className="price-container">
+                <input
+                required
+                id="price"
+                type="text"
+                name="price"
+                onChagne={this.handleChange}
+                placeholder="$price"
+                />
+              </div>
             </div>
           </div>
           <div className="column-half">
-            <input
-            required
-            id="title"
-            type="text"
-            name="title"
-            onChange={this.handleChange}
-            />
-            <hr/>
-            <input
-            required
-            id="description"
-            type="text"
-            name="description"
-            onChange={this.handleChange}
-            />
+            <div className="row center margin-top-1rem">
+              <div>
+                <div className="title-container">
+                  <input
+                  required
+                  id="title"
+                  type="text"
+                  name="title"
+                  onChange={this.handleChange}
+                  className="title"
+                  placeholder='title'
+                  />
+                </div>
+                <hr></hr>
+                <div className="description-container">
+                  <textarea
+                  required
+                  autoFocus
+                  id="description"
+                  type="text"
+                  name="description"
+                  onChange={this.handleChange}
+                  className="description"
+                  placeholder='description'
+                  />
+                </div>
+                <div className="row space-between margin-top-1rem">
+                  <a href="myprofile" ><button type="submit" className="upload-button">Upload</button></a>
+                  <a href="#myprofile"><button className="cancel-button">Cancel</button></a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </form>
